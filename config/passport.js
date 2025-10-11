98bf76446107f7ed80af24accf45a8c11af103d9
metalpadawan <emmanuelndem9@gmail.com>
2025-10-11 22:45:50 +0100

    updated passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,47 +1,49 @@
 // config/passport.js
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/user');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/user");
 
-module.exports = function(passportInstance) {
-  passportInstance.serializeUser((user, done) => done(null, user._id));
-  passportInstance.deserializeUser(async (id, done) => {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (err) {
-      done(err);
-    }
-  });
+module.exports = function (passport) {
+  // ✅ No serializeUser / deserializeUser needed since we aren’t using sessions
 
-  passportInstance.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback'
-  }, async (accessToken, refreshToken, profile, done) => {
-    try {
-      const email = profile.emails?.[0]?.value;
-      let user = await User.findOne({ provider: 'google', providerId: profile.id });
-      if (!user) {
-        // If an account with same email exists, link or use existing (optional)
-        user = await User.findOne({ email });
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL || "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const email = profile.emails?.[0]?.value;
+
+          // Try to find an existing user
+          let user = await User.findOne({
+            $or: [
+              { provider: "google", providerId: profile.id },
+              { email },
+            ],
+          });
+
+          // If user doesn’t exist, create a new one
+          if (!user) {
+            user = await User.create({
+              provider: "google",
+              providerId: profile.id,
+              email,
+              username: profile.displayName,
+            });
+          } else {
+            // Update provider info if missing
+            user.provider = "google";
+            user.providerId = profile.id;
+            await user.save();
+          }
+
+          return done(null, user);
+        } catch (err) {
+          return done(err, false);
+        }
       }
-      if (!user) {
-        user = await User.create({
-          provider: 'google',
-          providerId: profile.id,
-          email,
-          username: profile.displayName
-        });
-      } else {
-        // ensure providerId set if linking
-        user.provider = 'google';
-        user.providerId = profile.id;
-        await user.save();
-      }
-      done(null, user);
-    } catch (err) {
-      done(err);
-    }
-  }));
+    )
+  );
 };
